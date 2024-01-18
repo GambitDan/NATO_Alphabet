a0d38493061d66219b7a46382931329c8b030028
Daniel Houghton <danielhoughton1986@gmail.com>
2024-01-18 21:30:57 +0000

    Error handling added

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-word = input("Enter a word: ").upper()
-outputlist = [phonetic_dict[letter] for letter in word]
-print(outputlist)
+
+def convert():
+    word = input("Enter a word: ").upper()
+    try:
+        outputlist = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alaphbet")
+        convert()
+    else:
+        print(outputlist)
+
+
+convert()
